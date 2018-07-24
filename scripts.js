3218a0e5d9821bab0abeb1f94ed170d0cf68b457
peijia24 <peijia.dng@gmail.com>
2018-07-23 18:19:46 -0700

    responsive for all pages

--- scripts.js
+++ scripts.js
@@ -1,15 +1,5 @@
-// function openNav() {
-// 	document.getElementById("thisSidemenu").style.width ="100%";
-
-// }
-
-// function closeNav() {
-// 	document.getElementById("thisSidemenu").style.width="0";
-// }
-
-
     $(document).ready(function() {
         $('#showmenu').click(function() {
-                $('.menu').slideToggle("down");
+                $('.menu').slideToggle("slow");
         });
     });
